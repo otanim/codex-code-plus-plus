692b52bc24edf1d28517c5a6136ad119029a07f8
Arman Yeghiazaryan <otanim617@gmail.com>
2025-07-28 03:03:16 +0400

    Update hide-popover.user.js

--- hide-popover.user.js
+++ hide-popover.user.js
@@ -1,9 +1,8 @@
 // ==UserScript==
 // @name         Hide Popover via CSS
 // @namespace    http://tampermonkey.net/
-// @version      0.1.3
-// @author       Arman Yeghiazaryan - @otanim
-// @description  Hide the "Ctrl" popover by targeting the body's last or pre-last div element
+// @version      0.1
+// @description  Hide the script‑adjacent .popover.select-none by injecting CSS
 // @match        https://chatgpt.com/*
 // @run-at       document-start
 // @grant        none
@@ -12,14 +11,14 @@
 (function() {
     'use strict';
 
-    // inject a CSS rule so that the popover shown when holding Ctrl
-    // is hidden whether it's the last <div> or the second to last element if that element is a <div>
+    // inject a CSS rule so that any .popover.select-none immediately
+    // following a <script> tag is hidden by default
     const style = document.createElement('style');
     style.textContent = `
-        body > div.popover.select-none:last-child,
-        body > div.popover.select-none:nth-last-child(2) {
+        .popover.select-none:not([dir="ltr"]) {
             display: none !important;
         }
     `;
     document.head.appendChild(style);
+    console.log(style);
 })();
