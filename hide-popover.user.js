08943ac1b5ab9a1830721c13c4dec1aef9fa0789
Arman Yeghiazaryan <otanim617@gmail.com>
2025-07-28 02:54:51 +0400

    fix: hide last or pre-last popover

--- hide-popover.user.js
+++ hide-popover.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         Hide Popover via CSS
 // @namespace    http://tampermonkey.net/
-// @version      0.1.1
+// @version      0.1.2
 // @author       Arman Yeghiazaryan - @otanim
-// @description  Hide the "Ctrl" popover by targeting body's pre-last div
+// @description  Hide the "Ctrl" popover by targeting body's last or pre-last div
 // @match        https://chatgpt.com/*
 // @run-at       document-start
 // @grant        none
@@ -12,10 +12,11 @@
 (function() {
     'use strict';
 
-    // inject a CSS rule so that only body's second to last <div>
-    // (the popover shown when holding Ctrl) is hidden
+    // inject a CSS rule so that the popover shown when holding Ctrl
+    // is hidden whether it's the last or second to last <div>
     const style = document.createElement('style');
     style.textContent = `
+        body > div:nth-last-of-type(1).popover.select-none,
         body > div:nth-last-of-type(2).popover.select-none {
             display: none !important;
         }
